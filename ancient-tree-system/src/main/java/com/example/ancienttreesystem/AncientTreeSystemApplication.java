5c85d494717921b247ead62023f204323a02d5b3
ttzzff123 <64505337+ttzzff123@users.noreply.github.com>
2026-04-20 09:30:09 +0800

    Add files via upload

---
+++ ancient-tree-system/src/main/java/com/example/ancienttreesystem/AncientTreeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ancienttreesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncientTreeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AncientTreeSystemApplication.class, args);
+    }
+
+}
---
+++ ancient-tree-system/src/test/java/com/example/ancienttreesystem/AncientTreeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ancienttreesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AncientTreeSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
